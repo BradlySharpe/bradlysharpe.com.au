8da2d8731518e2a2de4ceff21d34512e957a00e6
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-04-07 17:52:45 +1000

    Fix company logo

--- _includes/Company.js
+++ _includes/Company.js
@@ -2,7 +2,7 @@
 { "@context" : "http://schema.org",
   "@type" : "Organization",
   "url" : "{{ site.baseurl | prepend: site.url }}",
-  "logo" : "{% if site.images_url != "" %}{{ site.images_url }}/images{% else %}{{ site.baseurl | prepend: site.url }}{% endif %}/bradly-sharpe-logo.png",
+  "logo" : "{% if site.images_url != "" %}{{ site.images_url }}{% else %}{{ site.baseurl | prepend: site.url }}{% endif %}/images/bradly-sharpe-logo.png",
   "contactPoint" : [
     { "@type" : "ContactPoint",
       "telephone" : "{{ site.contactNumber }}",

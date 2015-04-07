9b837d0d805ebd494c9d9308823c94757b074308
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-04-07 17:49:48 +1000

    Added logo to company

--- _includes/Company.js
+++ _includes/Company.js
@@ -2,6 +2,7 @@
 { "@context" : "http://schema.org",
   "@type" : "Organization",
   "url" : "{{ site.baseurl | prepend: site.url }}",
+  "logo" : "{{ site.images_url }}/images/bradly-sharpe-logo.png",
   "contactPoint" : [
     { "@type" : "ContactPoint",
       "telephone" : "{{ site.contactNumber }}",

a484dafccd84d6cb988305554dfd94dc6b8da301
Bradly Sharpe <brad7928@live.com.au>
2015-04-08 14:46:48 +1000

    Fix stylesheets

--- _includes/appendStyleSheets.js
+++ _includes/appendStyleSheets.js
@@ -9,64 +9,50 @@
 	  raleway: "{{ "/css/raleway.css" | prepend: site.assets_url }}",
 	  style: "{{ stylesheetName | prepend: site.assets_url }}",
 	  styleKey : "{{ page.url | replace:'/','_' }}",
-	  head: document.getElementsByTagName('head')[0],
-	  _localStorageSupported : function() {
-	    try {
-	      localStorage.setItem('test', 'test');
-	      localStorage.removeItem('test');
-	      return true;
-	    } 
-	    catch(e) { return false; }
-	  },
-	  _fetchAndSet : function(key, href) {
-	    var xhr = new XMLHttpRequest();
-	    xhr.open('GET', href, true);
-	    xhr.onreadystatechange = function() {
-	      if (xhr.readyState === 4) {
-	        BSStyleSheets._injectStyle(xhr.responseText);
-	        localStorage.setItem(key, xhr.responseText);
-	      }
-	    }
-	    xhr.send();
-	  },
-	  _injectStyle: function(text) {
-	    var style = document.createElement('style');
-	    style.innerHTML = text;
-	    this.head.appendChild(style);
-	  },
-	  _checkStyleLoaded : function(callback, href) {
-	    var defined,
-	      sheets = window.document.styleSheets;
-	    for(var i = 0, j = sheets.length; i < j; i++)
-	      defined = defined || (sheets[i].href && sheets[i].href.indexOf(href) > -1);
-	    if(defined) callback();
-	    else setTimeout(function() { BSStyleSheets._checkStyleLoaded(callback, href); });
-	  },
-	  _fetchStyle : function(href) {
-	    var l = document.createElement('link'); 
-	    l.rel = 'stylesheet';
-	    l.media = "only x";
-	    l.href = href;
-	    this.head.appendChild(l);
-	    this._checkStyleLoaded(function() { l.media = "all"; }, href)
-	  },
-	  _load : function(key, href) {
-			if (this._localStorageSupported()) {
-				if (localStorage[key]) this._injectStyle(localStorage.getItem(key));
-				else this._fetchAndSet(key, href);
-			} else {
-	      var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-	        webkitRequestAnimationFrame || msRequestAnimationFrame;
-	      var callback = function() { BSStyleSheets._fetchStyle(href); };
-	      if (raf) raf(callback);
-	      else window.addEventListener('load', callback);
-	    }
-	  },
-	  appendStyles : function() {
-	    this._load("raleway", this.raleway);
-	    this._load(this.styleKey, this.style);
-	  }
-	}
-
-	BSStyleSheets.appendStyles();
+    head: documenlink.getElementsByTagName("head")[0],
+    _localStorageSupported: function() {
+			try {
+				return localStorage.setItem("test", "test"), localStorage.removeItem("test"), !0
+			} catch (e) {
+				return !1
+			}
+    },
+    _fetchAndSet: function(key, href) {
+			var xhr = new XMLHttpRequest;
+			xhr.open("GET", href, !0), xhr.onreadystatechange = function() {
+				4 === xhr.readyState && (BSStyleSheets._injectStyle(xhr.responseText), localStorage.setItem(key, xhr.responseText))
+			}, xhr.send()
+    },
+    _injectStyle: function(text) {
+      var style = document.createElement("style");
+      style.innerHTML = text, this.head.appendChild(style)
+    },
+    _checkStyleLoaded: function(callback, href) {
+      for (var defined, stylesheets = window.document.styleSheets, o = 0, l = stylesheetslength; l > o; o++) 
+        defined = defined || stylesheets[o].href && stylesheets[o].href.indexOf(t) > -1;
+      defined ? callback() : setTimeout(function() {
+        BSStyleSheets._checkStyleLoaded(callback, href)
+      })
+    },
+    _fetchStyle: function(href) {
+      var link = document.createElement("link");
+      link.rel = "stylesheet", link.media = "only x", link.href = href, this.head.appendChild(link), this._checkStyleLoaded(function() {
+        link.media = "all"
+      }, href)
+    },
+    _load: function(key, href) {
+      if (this._localStorageSupported()) localStorage[key] ? this._injectStyle(localStorage.getItem(key)) : this._fetchAndSet(key, href);
+      else {
+        var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame,
+          callback = function() {
+            BSStyleSheets._fetchStyle(href)
+        	};
+        raf ? raf(callback) : window.addEventListener("load", callback)
+      }
+    },
+    appendStyles: function() {
+      this._load("raleway", this.raleway), this._load(this.styleKey, this.style)
+    }
+  };
+  BSStyleSheets.appendStyles();
 </script>
